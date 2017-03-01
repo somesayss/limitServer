58ccbdf9e75f661a4387fc1f42cd6faf2076ba87
somesayss <413461856@qq.com>
2017-03-01 16:22:26 +0800

    add

--- socket.js
+++ socket.js
@@ -10,26 +10,51 @@ const server = {};
 const wsList = [];
 const nameSpace = ['in'];
 
+/**
+ * 规则
+ * 客户端 => {to: 'others|thesys', value: 'something', type: 'type'}
+ * 服务端 => {from: 'others|thesys', value: 'something', type: 'type', id: 'o|1|2', displayName: '用户名'} //0代表系统，1代表用户
+ * 		
+ */
+
 const tellMap = {
-	// 告诉别人
-	tellthem(me, message, type){
-		limit(wsList).difference(me).forEach((ws) => {
-			ws.send( JSON.stringify({val: message, to: 'me', from: 'Them', fromName: getNameByWs(me), type: type}) );
+	// 别人的信息通知我
+	tellMeFromOthers(from, message, type){
+		limit(wsList).difference(from).forEach((ws) => {
+			let sendObj = {
+				from: 'others',
+				value: message,
+				type: type,
+				id: from._soId,
+				displayName: from._soDisplayName
+			};
+			ws.send(JSON.stringify(sendObj));
 		});
 	},
-	// 告诉所有人
-	tellall(message){
+	// 通知我
+	tellMe(message){
 		limit(wsList).forEach((ws) => {
 			ws.send(message);
 		});
 	},
-	// 回复我
-	tellme(ws, message, type){
-		ws.send( JSON.stringify({val: message, to: 'me', from: 'Sys', fromName: '系统', type: type}) );
+	// 系统通知我
+	tellMeFromThesys(ws, message, type){
+		let sendObj = {
+			from: 'thesys',
+			value: message,
+			type: type,
+			id: 0
+		};
+		ws.send(JSON.stringify(sendObj));
 	}
 };
 
-function inName(ws){
+const handles = {
+	thesys: {},
+	others: {}	
+};
+
+function setId(ws){
 	let pos = null;
 	limit.some(nameSpace, (val, key) => {
 		if( val === 'out' ){
@@ -42,18 +67,17 @@ function inName(ws){
 	}else{
 		nameSpace[pos] = 'in';
 	};
-	ws.POS = pos;
-	return pos;
+	ws._soId = pos;
 };
 
 function outName(ws){
-	nameSpace[ws.POS] = 'out';
+	nameSpace[ws._soId] = 'out';
 	// 回收
 	nameSpace.length = nameSpace.lastIndexOf('in') + 1;
 };
 
-function getNameByWs(ws){
-	return `游客${limit.padStart(ws.POS, 2, '0')}`;
+function upperName(name){
+	return name.replace(/^\w/, (a) => a.toUpperCase());
 };
 
 server.start = () => {
@@ -62,13 +86,16 @@ server.start = () => {
 		wsList.push(ws);
 		limit.inf(`链接数量:${wsList.length}`);
 		// 回复名称
-		inName(ws);
-		tellMap.tellme(ws, getNameByWs(ws), 'setName');
+		setId(ws);
+		tellMap.tellMeFromThesys(ws, ws._soId, 'tellId');
 	    ws.on('message', function (message) {
 	    	limit.log(`获取信息:${message}`);
 	    	try{
 	    		let msg = JSON.parse(message);
-	    		tellMap[`tell${msg.to}`](ws, msg.val);
+	    		msg = limit.cb( handles[msg.to][msg.type] )(msg);
+	    		if( msg ){
+	    			tellMap[`tellMeFrom${upperName(msg.to)}`](ws, msg.val, msg.type);
+	    		};
 	    	}catch(e){
 	    		limit.ERR(e);
 	    	};
